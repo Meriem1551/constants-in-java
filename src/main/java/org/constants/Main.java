cb200d3a8c5252856f31cdeeb1531733c7d33731
Meriem1551 <meriem200409@gmail.com>
2023-09-16 15:32:07 +0200

    update project

--- src/main/java/org/constants/Main.java
+++ src/main/java/org/constants/Main.java
@@ -2,6 +2,8 @@ package org.constants;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       final float PI = 3.14F; // constant
+        // we use capital letters for naming constant
+
     }
 }
